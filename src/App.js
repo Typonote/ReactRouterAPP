71dab9a3a84b8871d95977cbdda0b4e793bb0bdb
Typonote <kimradium@naver.com>
2021-11-09 14:05:36 +0900

    #4 URL 파라미터 사용하기 (p.335 ~)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, BrowserRouter, Routes, Link } from "react-router-dom";
 
 import Home from "./components/Home";
 import About from "./components/About";
+import Profile from "./components/Profile";
 
 function App() {
   return (
@@ -15,11 +16,18 @@ function App() {
           <li>
             <Link to="/about">소개</Link>
           </li>
+          <li>
+            <Link to="/profile/velopert">velopert 프로필</Link>
+          </li>
+          <li>
+            <Link to="/profile/gildong">gildong 프로필</Link>
+          </li>
         </ul>
         <hr />
         <Routes>
           <Route path="/" element={<Home />} exact={true} />
           <Route path="/about" element={<About />} />
+          <Route path="/profile/:username" element={<Profile />} />
         </Routes>
       </BrowserRouter>
     </div>

c3d78a96caed91690d43f83b36cece37576a180d
Typonote <kimradium@naver.com>
2021-11-09 11:57:08 +0900

    #2 페이지 생성 (p.328 ~)

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 
+// BrowserRouter => HTML5의 HISTORY API를 이용해 새로고침 하지 않아도 주소를 변경하고 조회할 수 있도록 해줌
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function

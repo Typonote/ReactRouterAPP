22c82f191ec1cdb7a6af6197a51a97f82f4bef42
Typonote <kimradium@naver.com>
2021-11-10 00:46:01 +0900

    #7 서브 라우트 설정 (p.339 ~)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Route, BrowserRouter, Routes, Link } from "react-router-dom";
 
 import Home from "./components/Home";
 import About from "./components/About";
+
+import Profiles from "./components/Profiles";
 import Profile from "./components/Profile";
 
 function App() {
@@ -17,17 +19,15 @@ function App() {
             <Link to="/about">소개</Link>
           </li>
           <li>
-            <Link to="/profile/velopert">velopert 프로필</Link>
-          </li>
-          <li>
-            <Link to="/profile/gildong">gildong 프로필</Link>
+            <Link to="/profiles">프로필</Link>
           </li>
         </ul>
         <hr />
         <Routes>
           <Route path="/" element={<Home />} exact={true} />
           <Route path="/about" element={<About />} />
-          <Route path="/profile/:username" element={<Profile />} />
+          <Route path="/profiles" element={<Profiles />} />
+          <Route path="/profiles/:username" element={<Profile />} />
         </Routes>
       </BrowserRouter>
     </div>
